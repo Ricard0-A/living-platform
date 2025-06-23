57148fbd5d5049e81d31cc861fa39c87d33aa315
ricardo <ricardoarias4@hotmail.com>
2025-06-23 16:18:12 -0500

    Hello World v2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,12 +7,17 @@ import Link from "next/link";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [openLang, setOpenLang] = useState(false);
 
   return (
     <nav className="bg-green-400text-white shadow-md">
       <div className="max-w-9xl px-6 py-4 flex items-center justify-between">
         <div className="flex space-x-5 text-lg font-500 hover:text-gray-200">
-          <div className="flex items-center gap-2">
+          {/* LANG */}
+          <div
+            className="cursor-pointer flex items-center gap-2"
+            onClick={() => setOpenLang(!openLang)}
+          >
             <h1>Languages</h1>
             <img
               className="h-4"
@@ -20,23 +25,38 @@ const Navbar = () => {
               alt="open box"
             />
           </div>
-
-          <div className="flex items-center">
-            <img
-              className="h-10"
-              src="./navbar-images/uk-language.png"
-              alt="eng"
-            />
-            <h2>English</h2>
-          </div>
-
-          <div className="flex items-center">
-            <img
-              className="h-10"
-              src="./navbar-images/spanish-language-icon.png"
-              alt="esp"
-            />
-            <h2>Spanish</h2>
+          {/* Caja padre de eng y esp */}
+          <div className="mt-2 left-0 flex space-x-4">
+            {/* ENG */}
+            <div
+              className={`flex items-center gap-2 p-2 rounded transition-all duration-300 ease-in-out ${
+                openLang
+                  ? "opacity-100 translate-x-0 scale-100 pointer-events-auto"
+                  : "opacity-50 -translate-x-10 scale-95 pointer-events-none"
+              }`}
+            >
+              <img
+                className="h-10"
+                src="./navbar-images/uk-language.png"
+                alt="eng"
+              />
+              <h2>English</h2>
+            </div>
+            {/* ESP */}
+            <div
+              className={`flex items-center gap-2  p-2 rounded transition-all duration-300 ease-in-out ${
+                openLang
+                  ? "opacity-100 translate-x-0 scale-100 pointer-events-auto"
+                  : "opacity-50 -translate-x-19 scale-95 pointer-events-none"
+              }`}
+            >
+              <img
+                className="h-10"
+                src="./navbar-images/spanish-language-icon.png"
+                alt="esp"
+              />
+              <h2>Spanish</h2>
+            </div>
           </div>
         </div>
 
@@ -82,19 +102,19 @@ const Navbar = () => {
           href="/"
           className="hover:text-gray-300 transition-colors duration-200"
         >
-          Inicio
+          Home
         </Link>
         <Link
           href="/about"
           className="hover:text-gray-300 transition-colors duration-200"
         >
-          Sobre mí
+          About us
         </Link>
         <Link
           href="/contact"
           className="hover:text-gray-300 transition-colors duration-200"
         >
-          Contacto
+          Contact
         </Link>
       </div>
     </nav>
