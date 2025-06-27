0c7947619187df7f4558c3c828465e9ca8e4998a
ricardo <ricardoarias4@hotmail.com>
2025-06-26 20:52:27 -0500

    Navbar and Home updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-// import Image from "next/image";
+import Image from "next/image";
 
 import { useState } from "react";
 import Link from "next/link";
@@ -10,10 +10,12 @@ const Navbar = () => {
   const [openLang, setOpenLang] = useState(false);
 
   return (
-    <nav className="bg-green-400text-white shadow-md">
-      <div className="max-w-9xl px-6 py-4 flex items-center justify-between">
+    <nav className="text-white shadow-md">
+      {/* Main Navbar Superior  */}
+      <div className="bg-gray-800 max-w-9xl px-5 flex items-center justify-between">
+        {/* Main Language Box   */}
         <div className="flex space-x-5 text-lg font-500 hover:text-gray-200">
-          {/* LANG */}
+          {/* Language  */}
           <div
             className="cursor-pointer flex items-center gap-2"
             onClick={() => setOpenLang(!openLang)}
@@ -25,42 +27,47 @@ const Navbar = () => {
               alt="open box"
             />
           </div>
-          {/* Caja padre de eng y esp */}
+          {/* Language Options ( ENG && ESP ) */}
           <div className="mt-2 left-0 flex space-x-4">
             {/* ENG */}
             <div
               className={`flex items-center gap-2 p-2 rounded transition-all duration-300 ease-in-out ${
                 openLang
                   ? "opacity-100 translate-x-0 scale-100 pointer-events-auto"
-                  : "opacity-50 -translate-x-10 scale-95 pointer-events-none"
+                  : "opacity-0 -translate-x-10 scale-95 pointer-events-none"
               }`}
             >
-              <img
-                className="h-10"
-                src="./navbar-images/uk-language.png"
-                alt="eng"
+              <Image
+                src="/navbar-images/uk-language.png"
+                alt="UK"
+                width={40}
+                height={40}
+                className="object-contain"
               />
               <h2>English</h2>
             </div>
+
             {/* ESP */}
             <div
               className={`flex items-center gap-2  p-2 rounded transition-all duration-300 ease-in-out ${
                 openLang
                   ? "opacity-100 translate-x-0 scale-100 pointer-events-auto"
-                  : "opacity-50 -translate-x-19 scale-95 pointer-events-none"
+                  : "opacity-0 -translate-x-19 scale-95 pointer-events-none"
               }`}
             >
-              <img
-                className="h-10"
-                src="./navbar-images/spanish-language-icon.png"
-                alt="esp"
+              <Image
+                src="/navbar-images/spanish-language-icon.png"
+                alt="ESP"
+                width={40}
+                height={40}
+                className="object-contain"
               />
               <h2>Spanish</h2>
             </div>
           </div>
         </div>
 
-        {/* Botón Hamburguesa */}
+        {/* Hamburger Bottom  ( For screens less than 768px ) */}
         <button
           className="md:hidden text-3xl"
           onClick={() => setIsOpen(!isOpen)}
@@ -69,8 +76,8 @@ const Navbar = () => {
           ☰
         </button>
 
-        {/* Menú Desktop */}
-        <div className="hidden md:flex space-x-8">
+        {/* Desktop Menu ( Applies at 768px and above  ) */}
+        <div className="hidden md:flex md:items-center space-x-8">
           <Link
             href="/"
             className="hover:text-gray-300 transition-colors duration-200"
@@ -83,16 +90,13 @@ const Navbar = () => {
           >
             About us
           </Link>
-          <Link
-            href="/contact"
-            className="hover:text-gray-300 transition-colors duration-200"
-          >
-            Contact
-          </Link>
+          <div className="font-bold p-2 bg-gray-500 text-green-200">
+            Sign In
+          </div>
         </div>
       </div>
 
-      {/* Menú Móvil */}
+      {/* Móvil Menu  ( For screens less than 768px )  */}
       <div
         className={`md:hidden flex flex-col space-y-2 px-6 overflow-hidden transition-all duration-300 ease-in-out ${
           isOpen ? "max-h-40 opacity-100 pt-2" : "max-h-0 opacity-0"
@@ -117,8 +121,22 @@ const Navbar = () => {
           Contact
         </Link>
       </div>
+
+      {/* Main Navbar Inferior */}
+      <div className="relative z-10 max-w-160 flex justify-end pe-7 py-2 ml-auto">
+        {/* Absolute + inset === 1/2 Transparent Background  */}
+        <div className="absolute inset-0 bg-black opacity-15 -z-10 rounded-lg" />
+        <ul className="z-10 flex asdsa items-center space-x-8 text-lg text-white ">
+          <li>Browse properties</li>
+          <li>Buy</li>
+          <li>Rent</li>
+          <li>How it works</li>
+          <div className="bg-blue-700 p-2">
+            <li>Get Evaluation</li>
+          </div>
+        </ul>
+      </div>
     </nav>
   );
 };
-
 export default Navbar;
