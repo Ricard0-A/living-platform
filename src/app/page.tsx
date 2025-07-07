eb699c2312e9c4e9886c7480f68c74c4f4d252de
ricardo <ricardoarias4@hotmail.com>
2025-07-07 16:30:19 -0500

    responsive navbar and home update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,93 @@
 "use client";
 import Image from "next/image";
 
-import { url } from "inspector";
+// import { url } from "inspector";
 
 export default function Home() {
   return (
     //  My Home
     <div
-      className="h-200 w-full mt-[-3.9rem]"
+      className="h-200 w-full mt-[-5rem] overflow-hidden"
       style={{
         backgroundImage: "url(./uk-home-opacity-50.png)",
         backgroundPosition: "center",
       }}
     >
-      {/* Features Nav */}
-      <div className="flex justify-between">
-        {/* Logo Box  */}
-        <div className="w-60 h-60 relative -top-8">
-          <Image alt="Lively Place Logo" fill src={"/logo-brand.png"} />
+      {/* Logo Box  */}
+      <div className="z-80 relative -start-4 top-15 w-47 h-47 md:-top-8 md:left-1 md:h-60 md:w-60 ">
+        <Image
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          alt="Lively Place Logo"
+          fill
+          src={"/logo-brand.png"}
+        />
+      </div>
+
+      {/* Main Title */}
+      <div className="w-full flex justify-center mt-18 ps-5">
+        <h1
+          style={{ wordSpacing: "0.5rem" }}
+          className="font-serif  text-6xl text-center"
+        >
+          Find your homeplace
+        </h1>
+      </div>
+
+      {/* Navigation ( Top )  */}
+      <section className="-ms-15">
+        <div className="w-full flex justify-center mt-20">
+          <div className="me-5 flex bg-transparent justify-center w-180 h-[2.7rem] gap-3 ms-44">
+            <button className="flex-1 bg-[#0025AF] text-lg rounded ">
+              Buy
+            </button>
+            <button className="flex-1 bg-[#1B40CA] text-lg rounded ">
+              Rent
+            </button>
+          </div>
         </div>
 
-        {/* Features */}
-      </div>
+        {/* Search Navigation */}
+        <div className="w-full flex justify-center ">
+          <form className="flex items-center w-220 h-20 bg-[#0025AF] rounded gap-5 px-4">
+            {/* ``By Cit`y` */}
+            <div className="flex items-center bg-white rounded-xl px-6 py-[5px]">
+              <h2 className="text-black text-base font-medium whitespace-nowrap">
+                By city
+              </h2>
+              <div className="relative w-[28px] h-[28px] ml-1">
+                <Image
+                  alt="down arrow"
+                  src="/black-downward-arrow.png"
+                  fill
+                  sizes="28px"
+                />
+              </div>
+            </div>
+
+            {/* Input */}
+            {/* flex-1 (flex:grow/shrink) siempre intenta tomar el 100% de su padre, 
+            y si otras cajas intervienen se acomoda sin problemas  */}
+            <div className="flex-1 bg-white h-[53%] rounded flex items-center px-4">
+              <input
+                className="w-full focus:outline-none text-black text-base"
+                type="text"
+                placeholder="Say something cool..."
+              />
+            </div>
+
+            {/* Search button */}
+            <button className="bg-[#0B0E52] flex justify-center items-center  text-white rounded-xl whitespace-nowrap">
+              <p className="text-lg font-medium ps-3">Search</p>
+              <Image
+                src="/glass-icon.png"
+                width={50}
+                height={50}
+                alt="Go for it"
+              />
+            </button>
+          </form>
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,10 @@
 "use client";
 import Image from "next/image";
+import { ChevronDown, ChevronRight } from "lucide-react";
+import { Menu, X } from "lucide-react";
 
 import { useState } from "react";
+
 import Link from "next/link";
 // import clsx from "clsx"; // Luego para condicionales en cadena
 
@@ -10,133 +13,168 @@ const Navbar = () => {
   const [openLang, setOpenLang] = useState(false);
 
   return (
-    <nav className="text-white shadow-md">
-      {/* Main Navbar Superior  */}
-      <div className="bg-gray-800 max-w-9xl px-5 flex items-center justify-between">
-        {/* Main Language Box   */}
-        <div className="flex space-x-5 text-lg font-500 hover:text-gray-200">
-          {/* Language  */}
-          <div
-            className="cursor-pointer flex items-center gap-2"
-            onClick={() => setOpenLang(!openLang)}
-          >
-            <h1>Languages</h1>
-            <img
-              className="h-4"
-              src="./navbar-images/play-button-icon.png"
-              alt="open box"
-            />
-          </div>
-          {/* Language Options ( ENG && ESP ) */}
-          <div className="mt-2 left-0 flex space-x-4">
-            {/* ENG */}
+    <>
+      <nav className="hidden md:block shadow-md text-white ">
+        {/* Main Navbar ( Top )  */}
+        <div className="bg-gray-800 max-w-9xl px-5 flex items-center justify-between">
+          {/* Main Language Box   */}
+          <div className="flex space-x-5 text-lg font-500 hover:text-gray-200">
+            {/* Language  */}
             <div
-              className={`flex items-center gap-2 p-2 rounded transition-all duration-300 ease-in-out ${
-                openLang
-                  ? "opacity-100 translate-x-0 scale-100 pointer-events-auto"
-                  : "opacity-0 -translate-x-10 scale-95 pointer-events-none"
-              }`}
+              className="cursor-pointer flex items-center gap-2"
+              onClick={() => setOpenLang(!openLang)}
             >
-              <Image
-                src="/navbar-images/uk-language.png"
-                alt="UK"
-                width={40}
-                height={40}
-                className="object-contain"
+              <h1>Languages</h1>
+              <img
+                className="h-4"
+                src="./navbar-images/play-button-icon.png"
+                alt="open box"
               />
-              <h2>English</h2>
             </div>
+            {/* Language Options ( ENG && ESP )  (Left side ) */}
+            <div className="mt-2 left-0 flex space-x-4">
+              {/* ENG */}
+              <div
+                className={`flex items-center gap-2 p-2 rounded transition-all duration-300 ease-in-out ${
+                  openLang
+                    ? "opacity-100 translate-x-0 scale-100 pointer-events-auto"
+                    : "opacity-0 -translate-x-10 scale-95 pointer-events-none"
+                }`}
+              >
+                <div className="relative w-[40px] h-[40px]">
+                  <Image
+                    src="/navbar-images/uk-language.png"
+                    alt="UK flag"
+                    fill
+                    sizes="40px"
+                  />
+                </div>
+                <h2>English</h2>
+              </div>
 
-            {/* ESP */}
-            <div
-              className={`flex items-center gap-2  p-2 rounded transition-all duration-300 ease-in-out ${
-                openLang
-                  ? "opacity-100 translate-x-0 scale-100 pointer-events-auto"
-                  : "opacity-0 -translate-x-19 scale-95 pointer-events-none"
-              }`}
+              {/* ESP */}
+              <div
+                className={`flex items-center gap-2  p-2 rounded transition-all duration-300 ease-in-out ${
+                  openLang
+                    ? "opacity-100 translate-x-0 scale-100 pointer-events-auto"
+                    : "opacity-0 -translate-x-19 scale-95 pointer-events-none"
+                }`}
+              >
+                <Image
+                  src="/navbar-images/spanish-language-icon.png"
+                  alt="ESP flag"
+                  width={40}
+                  height={40}
+                  className="object-contain"
+                />
+                <h2>Spanish</h2>
+              </div>
+            </div>
+          </div>
+
+          {/* Desktop Menu ( Applies at 768px and above ) ( Right side )  */}
+          <div className="hidden md:flex md:items-center space-x-8">
+            <Link
+              href="/"
+              className="hover:text-gray-300 transition-colors duration-200"
             >
-              <Image
-                src="/navbar-images/spanish-language-icon.png"
-                alt="ESP"
-                width={40}
-                height={40}
-                className="object-contain"
-              />
-              <h2>Spanish</h2>
+              Home
+            </Link>
+            <Link
+              href="/about"
+              className="hover:text-gray-300 transition-colors duration-200"
+            >
+              About us
+            </Link>
+            <div className="font-bold px-4 p-2 bg-gray-500 text-green-200">
+              Sign In
             </div>
           </div>
         </div>
 
-        {/* Hamburger Bottom  ( For screens less than 768px ) */}
-        <button
-          className="md:hidden text-3xl"
-          onClick={() => setIsOpen(!isOpen)}
-          aria-label="Toggle menu"
-        >
-          ☰
-        </button>
-
-        {/* Desktop Menu ( Applies at 768px and above  ) */}
-        <div className="hidden md:flex md:items-center space-x-8">
-          <Link
-            href="/"
-            className="hover:text-gray-300 transition-colors duration-200"
-          >
-            Home
-          </Link>
-          <Link
-            href="/about"
-            className="hover:text-gray-300 transition-colors duration-200"
-          >
-            About us
-          </Link>
-          <div className="font-bold p-2 bg-gray-500 text-green-200">
-            Sign In
-          </div>
+        {/* Navbar ( Bottom )  */}
+        <div className="relative -top-1 z-10 overflow-hidden max-h-[5rem] max-w-[41rem] ml-auto px-4 py-3">
+          {/* Transparent overlay */}
+          <div className="absolute inset-0 bg-black opacity-15 -z-10 rounded-lg" />
+          <ul className="z-10 flex items-center justify-end gap-6 text-lg text-white pe-4">
+            <li>
+              <div className="flex items-center gap-2">
+                <h2> Browse properties</h2>
+                <ChevronDown color="white" size="20" />
+              </div>
+            </li>
+            <li>Buy</li>
+            <li>Rent</li>
+            <li>How it works</li>
+            <li>
+              <div className="bg-blue-700 px-3 py-3 rounded">
+                Get Evaluation
+              </div>
+            </li>
+          </ul>
         </div>
-      </div>
-
-      {/* Móvil Menu  ( For screens less than 768px )  */}
+      </nav>
+      {/* Open/Close Mobile Navbar */}
       <div
-        className={`md:hidden flex flex-col space-y-2 px-6 overflow-hidden transition-all duration-300 ease-in-out ${
-          isOpen ? "max-h-40 opacity-100 pt-2" : "max-h-0 opacity-0"
-        }`}
+        className={`fixed top-4 right-4 z-60 cursor-pointer md:hidden`}
+        onClick={() => setIsOpen(!isOpen)}
       >
-        <Link
-          href="/"
-          className="hover:text-gray-300 transition-colors duration-200"
-        >
-          Home
-        </Link>
-        <Link
-          href="/about"
-          className="hover:text-gray-300 transition-colors duration-200"
-        >
-          About us
-        </Link>
-        <Link
-          href="/contact"
-          className="hover:text-gray-300 transition-colors duration-200"
-        >
-          Contact
-        </Link>
+        {isOpen ? (
+          <X className="scale-250" color="white" size={20} />
+        ) : (
+          <Menu className="scale-250" color="white" size={20} />
+        )}
       </div>
 
-      {/* Main Navbar Inferior */}
-      <div className="relative z-10 max-w-160 flex justify-end pe-7 py-2 ml-auto">
-        {/* Absolute + inset === 1/2 Transparent Background  */}
-        <div className="absolute inset-0 bg-black opacity-15 -z-10 rounded-lg" />
-        <ul className="z-10 flex asdsa items-center space-x-8 text-lg text-white ">
-          <li>Browse properties</li>
-          <li>Buy</li>
-          <li>Rent</li>
-          <li>How it works</li>
-          <div className="bg-blue-700 p-2">
-            <li>Get Evaluation</li>
-          </div>
-        </ul>
-      </div>
-    </nav>
+      {/* Mobile Navbar*/}
+      <nav
+        className={`fixed inset-0 bg-black text-white z-50 transition-all duration-600 ease-in-out 
+    transform ${
+      isOpen ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"
+    }`}
+      >
+        <div className="pt-40 w-full">
+          <ul className="flex flex-col gap-6 text-xl p-4">
+            <li className="flex items-center space-x-2">
+              <span>Browse properties</span>
+              <ChevronRight color="white" size={20} />
+            </li>
+            <li className="flex items-center space-x-2">
+              <span>Buy</span>
+              <ChevronRight color="white" size={20} />
+            </li>
+            <li className="flex items-center space-x-2">
+              <span>Rent</span>
+              <ChevronRight color="white" size={20} />
+            </li>
+            <li className="flex items-center space-x-2">
+              <span>How it works</span>
+              <ChevronRight color="white" size={20} />
+            </li>
+            <li>
+              <div className="w-[50%] h-0.5 bg-white" />
+            </li>
+            <li className="flex items-center space-x-2">
+              <span>Languages</span>
+              <ChevronRight color="white" size={20} />
+            </li>
+            <li className="flex items-center space-x-2">
+              <span>Contact us</span>
+              <ChevronRight color="white" size={20} />
+            </li>
+            <li className="flex items-center space-x-2">
+              <span>Get Evaluation</span>
+              <ChevronRight color="white" size={20} />
+            </li>
+            <li>
+              <div className="mt-13 flex items-center justify-center mx-auto p-4 w-[52%]  bg-[#0025AF]">
+                <h2 className="text-1xl">Sign In</h2>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 };
 export default Navbar;
